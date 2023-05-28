39c9d4b468db47fc7a640687ab306f88ca7241ac
Christoph Kruppe <c.kruppe@nfq.de>
2023-05-28 10:50:42 +0200

    feat(Cypress): Add rule exeptions for cypress tests

---
+++ rules/typescript-cypress-exeptions.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-inline-comments */
+module.exports = {
+    rules: {
+        '@nfq/no-magic-numbers': 'off',
+        'max-len': 'off',
+        'max-lines': 'off',
+        'max-lines-per-function': 'off',
+        'newline-per-chained-call': 'off',
+        'promise/always-return': 'off',
+        'promise/catch-or-return': 'off',
+        'promise/prefer-await-to-then': 'off'
+    }
+};
